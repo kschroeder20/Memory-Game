60886fdb1d08c644afce6665fa4ce38b52056ac9
Kevin Schroeder <schroederkevin94@gmail.com>
2019-07-21 10:01:26 -0400

    Update Jumbotron.js

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -6,7 +6,7 @@ class Jumbotron extends Component {
         return (
             <div id="jumbotronContent" className="jumbotron jumbotron-fluid container">
                 <div>
-                    <h1 className="display-10" id="instructionHeadline">Remeber the Logo</h1>
+                    <h1 className="display-10" id="instructionHeadline">Remember the Logo</h1>
                     <p className="lead" id="instructionParagraph">
                         Click on an logo to earn points, but don't click on any of the logos more than
                         once! See if you can click on all 12 without repeating.
